9c40cf30290485d1248f044b8e797c563e65706e
Nate Brito <nathan.brito@mottu.com.br>
2021-09-12 19:13:10 -0300

    classes one and two

--- class_one/src/app/app.component.ts
+++ class_one/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any;
+
+  transferir($event) {
+    console.log(
+      '🚀 ~ file: app.component.ts ~ line 11 ~ AppComponent ~ transferir ~ $event',
+      $event
+    );
+    this.transferencia = $event;
+  }
+  exibirModalErro(mensagem) {
+    alert(mensagem);
+  }
 }
--- class_one/src/app/nova-transferencia/nova-transferencia.component.ts
+++ class_one/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+  @Output() valoresComErro = new EventEmitter<string>();
+  valor: number;
+  destino: number;
+  transferir() {
+    console.log('Solicitada nova transferência');
 
+    if (this.ehValido()) {
+      let valorEmitdo = { valor: this.valor, destino: this.destino };
+      this.aoTransferir.emit(valorEmitdo);
+      this.limparCampos();
+    }
+  }
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+
+  private ehValido() {
+    const valido = this.valor > 0 && this.destino > 0;
+    if (!valido) {
+      this.valoresComErro.emit('Informe um valor e um destino válido');
+    }
+    return valido;
+  }
 }
