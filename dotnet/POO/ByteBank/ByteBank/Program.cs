6d46d534646d3f38a66f9535a456f0c36cfeb4c7
Nate <nathan.brito@mottu.com.br>
2022-03-01 11:46:01 -0300

    ajustes no inicializador do obj

--- dotnet/POO/ByteBank/ByteBank/Program.cs
+++ dotnet/POO/ByteBank/ByteBank/Program.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ByteBank;
 
-ContaCorrente contaNate = new ContaCorrente();
-contaNate.agencia = 666;
-contaNate.numero = 666369;
-contaNate.saldo = 2.99;
-contaNate.titular = "Nate";
+ContaCorrente contaNate = new ContaCorrente
+{
+    agencia = 666,
+    numero = 666369,
+    saldo = 2.99,
+    titular = "Nate"
+};
 Console.WriteLine($"Olá {contaNate.titular} bem-vindo ao ByteBank,\n" +
                   $"sua agencia é a {contaNate.agencia}\n" +
                   $"o numero da sua conta é o {contaNate.numero}\n" +
